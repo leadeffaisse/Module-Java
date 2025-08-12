4dffd9b8635718cf018e41b858facc5359a6551a
Léa Deffaisse <l.deffaisse@laposte.net>
2025-08-12 16:17:24 +0200

    First move

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,21 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Variables
+        int totalBoxes = 34;
+        int truckCapacity = 9;
+        int remainingBoxes = totalBoxes;
+        int movesCount = 0;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (remainingBoxes > 0) {
+            int boxesMoved;
+            if (remainingBoxes > truckCapacity) {
+                boxesMoved = truckCapacity;
+            } else {
+                boxesMoved = remainingBoxes;
+            }
+            remainingBoxes -= boxesMoved;
+            movesCount++;
+            System.out.printf("Voyage " + movesCount + " : " + boxesMoved + " cartons déplacés.");
         }
     }
 }
